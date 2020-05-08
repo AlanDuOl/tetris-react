1999db6fbf2c70e69d54a4f856ea3533fe99d6e3
alanduarte90 <alanduarte90@gmail.com>
2020-05-08 15:50:50 +0100

    Connect Menu to store and set game start state

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,11 +1,19 @@
 import React from 'react'
 import '../css/Menu.scss'
+import { connect } from 'react-redux'
+import { setGameOn } from '../actions/gameActions.js'
 
-function Menu() {
+function Menu(props) {
+
+    // Start the game and create block
+    function start() {
+        props.setGameOn(true)
+    }
+
     return (
         <section id="menu">
             <div className="menu-element" id="menu-element-1">
-                <button type="button" className="menu-btn" id="menu-btn-start">Start</button>
+                <button type="button" className="menu-btn" id="menu-btn-start" onClick={start}>Start</button>
             </div>
             <div className="menu-element" id="menu-element-2">
                 <button type="button" className="menu-btn" id="menu-btn-pause">Pause</button>
@@ -20,4 +28,8 @@ function Menu() {
     )
 }
 
-export default Menu
+const mapDispatchToProps = dispatch => ({
+    setGameOn: gameOn => dispatch(setGameOn(gameOn))
+})
+
+export default connect(null, mapDispatchToProps)(Menu)
