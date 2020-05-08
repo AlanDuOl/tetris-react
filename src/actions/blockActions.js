e37ed496f08a30e1a27805b3c50e3af79f327454
alanduarte90 <alanduarte90@gmail.com>
2020-05-08 22:56:06 +0100

    Add new actions

--- src/actions/blockActions.js
+++ src/actions/blockActions.js
@@ -3,12 +3,22 @@ export const setBlockState = (actionType, stateValue) => {
 
     try {
         switch (actionType) {
+            case "SET_BLOCK_TURN":
+                return { type: "SET_BLOCK_TURN", value: stateValue}
+            case "SET_BLOCK_MOVE_LEFT":
+                return { type: "SET_BLOCK_MOVE_LEFT", value: stateValue}
+            case "SET_BLOCK_MOVE_RIGHT":
+                return { type: "SET_BLOCK_MOVE_RIGHT", value: stateValue}
+            case "SET_BLOCK_SPEED_UP":
+                return { type: "SET_BLOCK_SPEED_UP", value: stateValue}
             case "SET_BLOCK_SPEED":
                 return { type: "SET_BLOCK_SPEED", value: stateValue}
             case "SET_BLOCK_ON":
                 return { type: "SET_BLOCK_ON", value: stateValue}
             case "SET_BLOCK_POSITION":
                 return { type: "SET_BLOCK_POSITION", value: stateValue}
+            case "SET_BLOCK_POSITION":
+                return { type: "SET_BLOCK_ROTATION", value: stateValue}
             case "SET_BLOCK_STATE":
                 return { type: "SET_BLOCK_STATE", value: stateValue}
             default:
