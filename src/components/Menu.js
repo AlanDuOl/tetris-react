496e936f0884aeba07ed5890ab95353d891cadf2
alanduarte90 <alanduarte90@gmail.com>
2020-05-10 00:40:15 +0100

    Change btn names from toggles to btn

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -7,18 +7,32 @@ function Menu(props) {
 
     // Start the game and create block
     function toggleStart(evt) {
-        // console.log(evt.target)
         try {
             if (!props.gameReducer.gameOn) {
                 props.setGameState("SET_GAME_ON", true)
-                evt.target.textContent = "Finish"
             }
-            else {
+            else if (props.gameReducer.gameOn) {
+                // TODO: implement custom window for confirm
                 let quit = window.confirm("Finish?")
                 if (quit) {
                     props.setGameState("SET_GAME_ON", false)
-                    evt.target.textContent = "Start"
+                    props.setGameState("SET_GAME_PAUSED", false)
                 }
+            }
+        }
+        catch(e) {
+            console.log(e.message)
+        }
+    }
+
+    function togglePause(evt) {
+        try {
+            if (!props.gameReducer.gamePaused && props.gameReducer.gameOn) {
+                props.setGameState("SET_GAME_PAUSED", true)
+            }
+            else if(props.gameReducer.gamePaused) {
+                // TODO: implement custom window for paused
+                props.setGameState("SET_GAME_PAUSED", false)
             }
         }
         catch(e) {
@@ -29,10 +43,10 @@ function Menu(props) {
     return (
         <section id="menu">
             <div className="menu-element" id="menu-element-1">
-                <button type="button" className="menu-btn" id="menu-btn-start" onClick={toggleStart}>Start</button>
+                <button type="button" className="menu-btn" id="menu-btn-start" onClick={toggleStart}>{ props.gameReducer.gameOn ? "Finish" : "Start" }</button>
             </div>
             <div className="menu-element" id="menu-element-2">
-                <button type="button" className="menu-btn" id="menu-btn-pause">Pause</button>
+                <button type="button" className="menu-btn" id="menu-btn-pause" onClick={togglePause}>{ props.gameReducer.gamePaused ? "Resume" : "Pause" }</button>
             </div>
             <div className="menu-element" id="menu-element-3">
                 <button type="button" className="menu-btn" id="menu-btn-save">Save</button>
