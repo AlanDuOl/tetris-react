06e438101852d3f80d6621b0872879d8d16ad181
alanduarte90 <alanduarte90@gmail.com>
2020-05-09 22:02:14 +0100

    Remove try catch

--- src/actions/gameActions.js
+++ src/actions/gameActions.js
@@ -1,26 +1,21 @@
 
 export const setGameState = (actionType, actionValue) => {
 
-    try {
-        switch (actionType) {
-            case "SET_GAME_WALL":
-                return { type: "SET_GAME_WALL", value: actionValue }
-            case "SET_GAME_LEVEL":
-                return { type: "SET_GAME_LEVEL", value: actionValue }
-            case "SET_GAME_SCORE":
-                return { type: "SET_GAME_SCORE", value: actionValue }
-            case "SET_GAME_ON":
-                return { type: "SET_GAME_ON", value: actionValue }
-            case "SET_GAME_PAUSED":
-                return { type: "SET_GAME_PAUSED", value: actionValue }
-            case "SET_GAME_STATE":
-                return { type: "SET_GAME_STATE", value: actionValue }
-            default:
-                return { type: "", value: null };
-        }
+    switch (actionType) {
+        case "SET_GAME_WALL":
+            return { type: "SET_GAME_WALL", value: actionValue }
+        case "SET_GAME_LEVEL":
+            return { type: "SET_GAME_LEVEL", value: actionValue }
+        case "SET_GAME_SCORE":
+            return { type: "SET_GAME_SCORE", value: actionValue }
+        case "SET_GAME_ON":
+            return { type: "SET_GAME_ON", value: actionValue }
+        case "SET_GAME_PAUSED":
+            return { type: "SET_GAME_PAUSED", value: actionValue }
+        case "SET_GAME_STATE":
+            return { type: "SET_GAME_STATE", value: actionValue }
+        default:
+            return { type: "", value: null };
     }
-    catch(e) {
-        console.log(e.message)
-    } 
     
 }
