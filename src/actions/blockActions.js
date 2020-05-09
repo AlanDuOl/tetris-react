37a5551e14f7cb98455170f565ebb31bea5f27cc
alanduarte90 <alanduarte90@gmail.com>
2020-05-09 15:07:05 +0100

    Change action value in blockActions

--- src/actions/blockActions.js
+++ src/actions/blockActions.js
@@ -1,32 +1,27 @@
 
-export const setBlockState = (actionType, stateValue) => {
-
-    try {
-        switch (actionType) {
-            case "SET_BLOCK_TURN":
-                return { type: "SET_BLOCK_TURN", value: stateValue}
-            case "SET_BLOCK_MOVE_LEFT":
-                return { type: "SET_BLOCK_MOVE_LEFT", value: stateValue}
-            case "SET_BLOCK_MOVE_RIGHT":
-                return { type: "SET_BLOCK_MOVE_RIGHT", value: stateValue}
-            case "SET_BLOCK_SPEED_UP":
-                return { type: "SET_BLOCK_SPEED_UP", value: stateValue}
-            case "SET_BLOCK_SPEED":
-                return { type: "SET_BLOCK_SPEED", value: stateValue}
-            case "SET_BLOCK_ON":
-                return { type: "SET_BLOCK_ON", value: stateValue}
-            case "SET_BLOCK_POSITION":
-                return { type: "SET_BLOCK_POSITION", value: stateValue}
-            case "SET_BLOCK_POSITION":
-                return { type: "SET_BLOCK_ROTATION", value: stateValue}
-            case "SET_BLOCK_STATE":
-                return { type: "SET_BLOCK_STATE", value: stateValue}
-            default:
-                throw Error("Unknown block action type")
-        }
-    }
-    catch(e) {
-        console.log(e.message)
-    }
+export const setBlockState = (actionType, actionValue) => {
     
+    switch (actionType) {
+        case "SET_BLOCK_TURN":
+            return { type: "SET_BLOCK_TURN", value: actionValue }
+        case "SET_BLOCK_MOVE_LEFT":
+            return { type: "SET_BLOCK_MOVE_LEFT", value: actionValue }
+        case "SET_BLOCK_MOVE_RIGHT":
+            return { type: "SET_BLOCK_MOVE_RIGHT", value: actionValue }
+        case "SET_BLOCK_SPEED_UP":
+            return { type: "SET_BLOCK_SPEED_UP", value: actionValue }
+        case "SET_BLOCK_SPEED":
+            return { type: "SET_BLOCK_SPEED", value: actionValue }
+        case "SET_BLOCK_ON":
+            return { type: "SET_BLOCK_ON", value: actionValue }
+        case "SET_BLOCK_POSITION":
+            return { type: "SET_BLOCK_POSITION", value: actionValue }
+        case "SET_BLOCK_ROTATION":
+            return { type: "SET_BLOCK_ROTATION", value: actionValue }
+        case "SET_BLOCK_STATE":
+            return { type: "SET_BLOCK", value: actionValue }
+        default:
+            return { type: "", value: null }
+    }
+
 }
