03d0c10c9e04bc32411df4f3757867620c69759f
alanduarte90 <alanduarte90@gmail.com>
2020-05-11 01:44:40 +0100

    Remove unused actions

--- src/actions/blockActions.js
+++ src/actions/blockActions.js
@@ -8,18 +8,12 @@ export const setBlockState = (actionType, actionValue) => {
             return { type: "SET_BLOCK_MOVE_LEFT", value: actionValue }
         case "SET_BLOCK_MOVE_RIGHT":
             return { type: "SET_BLOCK_MOVE_RIGHT", value: actionValue }
-        case "SET_BLOCK_SPEED_UP":
-            return { type: "SET_BLOCK_SPEED_UP", value: actionValue }
         case "SET_BLOCK_SPEED":
             return { type: "SET_BLOCK_SPEED", value: actionValue }
-        case "SET_BLOCK_ON":
-            return { type: "SET_BLOCK_ON", value: actionValue }
         case "SET_BLOCK_POSITION":
             return { type: "SET_BLOCK_POSITION", value: actionValue }
         case "SET_BLOCK_ROTATION":
             return { type: "SET_BLOCK_ROTATION", value: actionValue }
-        case "SET_BLOCK_RESTART":
-            return { type: "SET_BLOCK_RESTART", value: actionValue }
         case "SET_BLOCK_STATE":
             return { type: "SET_BLOCK", value: actionValue }
         default:
--- src/reducers/blockReducer.js
+++ src/reducers/blockReducer.js
@@ -1,16 +1,13 @@
 import { BLOCK_DELTA_SPEED } from '../globals'
 
 const initialState = {
-    speedUp: false,
     turn: false,
     moveLeft: false,
     moveRight: false,
-    on: true,
     speed: BLOCK_DELTA_SPEED,
     number: 0,
     position: { left: 0, bottom: 0 },
-    rotation: 0,
-    restart: false
+    rotation: 0
 }
 
 const blockReducer = (state = initialState, action) => {
@@ -23,20 +20,14 @@ const blockReducer = (state = initialState, action) => {
             return Object.assign({}, state, { moveLeft: action.value })
         case "SET_BLOCK_MOVE_RIGHT":
             return Object.assign({}, state, { moveRight: action.value })
-        case "SET_BLOCK_SPEED_UP":
-            return Object.assign({}, state, { speedUp: action.value })
         case "SET_BLOCK_SPEED":
             return Object.assign({}, state, { speed: action.value })
-        case "SET_BLOCK_ON":
-            return Object.assign({}, state, { on: action.value })
         case "SET_BLOCK_POSITION":
             return Object.assign({}, state, { position: action.value })
         case "SET_BLOCK_ROTATION":
             return Object.assign({}, state, { rotation: action.value })
         case "SET_BLOCK_NUMBER":
             return Object.assign({}, state, { number: action.value })
-        case "SET_BLOCK_RESTART":
-            return Object.assign({}, state, { restart: action.value })
         case "SET_BLOCK":
             return Object.assign({}, state, action.value)
         default:
