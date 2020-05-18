51e9aac9306edb537bbe06d105cbc9ec92d60563
alanduarte90 <alanduarte90@gmail.com>
2020-05-18 21:07:36 +0100

    Add implement function to check for rotation collision

--- src/libs/blockLib.js
+++ src/libs/blockLib.js
@@ -57,8 +57,15 @@ export const blockNewRotation = currentRotation => {
     return rotation
 }
 
-export const checkSideMove = (block, viewportWidth) => {
-    
+export const checkRotationCollision = (block, viewportWidth) => {
+    if (block) {
+        let tileWidth = parseFloat(viewportWidth / NUM_TILES_WIDTH)
+        let blockRect = block.getBoundingClientRect()
+        let blockCurrentRight = parseFloat(block.style.left) + blockRect.width
+        if (blockCurrentRight > viewportWidth) {
+            block.style.left = `${parseFloat(block.style.left) - tileWidth}px`
+        }
+    }
 }
 
 export function blockNewSpeed(currentSpeed) {
@@ -87,38 +94,32 @@ function getBlockType() {
     let blockNumber = getBlockNumber()
 
     // Return the blockType that is going to be used as a css class to define the block layout
-    try {
-        switch(blockNumber) {
-            case 1:
-                blockType = blockTypes.one.name
-                return blockType
-            case 2:
-                blockType = blockTypes.two.name
-                return blockType
-            case 3:
-                blockType = blockTypes.three.name
-                return blockType
-            case 4:
-                blockType = blockTypes.four.name
-                return blockType
-            case 5:
-                blockType = blockTypes.five.name
-                return blockType
-            case 6:
-                blockType = blockTypes.six.name
-                return blockType
-            case 7:
-                blockType = blockTypes.seven.name
-                return blockType
-            default:
-                console.log("Unknown block number: " + blockNumber)
-                break
-        }
+    switch(blockNumber) {
+        case 1:
+            blockType = blockTypes.one.name
+            return blockType
+        case 2:
+            blockType = blockTypes.two.name
+            return blockType
+        case 3:
+            blockType = blockTypes.three.name
+            return blockType
+        case 4:
+            blockType = blockTypes.four.name
+            return blockType
+        case 5:
+            blockType = blockTypes.five.name
+            return blockType
+        case 6:
+            blockType = blockTypes.six.name
+            return blockType
+        case 7:
+            blockType = blockTypes.seven.name
+            return blockType
+        default:
+            console.log("Unknown block number: " + blockNumber)
+            break
     }
-    catch(e) {
-        console.log(e.message)
-    }
-    
 }
 
 // Return the block number used to define the block type
