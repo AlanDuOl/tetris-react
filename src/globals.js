7968880e348e89696a5b60639e81ec05e4b43e5d
alanduarte90 <alanduarte90@gmail.com>
2020-05-19 13:18:43 +0100

    Add block initial speed const

--- src/globals.js
+++ src/globals.js
@@ -3,6 +3,7 @@ export const NUM_BLOCK_TYPES = 7
 export const BLOCK_NUM_TILES = 4
 export const NUM_TILES_WIDTH = 12
 export const NUM_TILES_HEIGHT = 36
+export const BLOCK_INITIAL_SPEED = 5
 export const BLOCK_DELTA_SPEED = 5
 export const BLOCK_DELTA_ROTATION = 90
 
