8e93abd9fee73ae313d9dda95d27467edaee3276
alanduarte90 <alanduarte90@gmail.com>
2020-05-21 18:09:45 +0100

    Create action types constant

--- src/globals.js
+++ src/globals.js
@@ -6,12 +6,21 @@ export const NUM_TILES_HEIGHT = 36
 export const BLOCK_INITIAL_SPEED = 1
 export const BLOCK_DELTA_SPEED = 5
 export const BLOCK_DELTA_ROTATION = 90
+export const TIMER_SPEED = 50
 
-export const blockMoveDirections = {
+export const blockMoveDirection = {
     left: "left",
     right: "right",
     none: "none"
 }
+
+export const actionType = {
+    blockMove: "SET_BLOCK_MOVE",
+    blockSpeed: "SET_BLOCK_SPEED",
+    blockPosition: "SET_BLOCK_POSITION",
+    blockRotation: "SET_BLOCK_ROTATION"
+}
+
 
 export const blockTypes = {
     one: {
@@ -37,10 +46,10 @@ export const blockTypes = {
     }
 }
 
-export const timerSpeeds = {
-    level1: 400,
-    level2: 300,
-    level3: 200,
-    level4: 100,
-    speedUp: 50
-}
+// export const timerSpeeds = {
+//     level1: 400,
+//     level2: 300,
+//     level3: 200,
+//     level4: 100,
+//     speedUp: 50
+// }
