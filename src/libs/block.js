b9798b3c379244b5b674223d89440c7727a4003f
alanduarte90 <alanduarte90@gmail.com>
2020-05-21 18:08:27 +0100

    Move game functions to libs

--- src/libs/block.js
+++ src/libs/block.js
@@ -1,158 +1,215 @@
-import React from "react"
-import { blockTypes, BLOCK_NUM_TILES, NUM_TILES_WIDTH, BLOCK_DELTA_SPEED, timerSpeeds, BLOCK_DELTA_ROTATION, blockMoveDirections } from '../globals.js'
+import { BLOCK_INITIAL_SPEED, blockMoveDirection, actionType } from '../globals.js'
 
 
-export const setBlockInitialPosition = (block, viewportWidth, viewportHeight) => {
-    if (block) {
-        let tileWidth = viewportWidth / NUM_TILES_WIDTH
-        block.style.left = `${tileWidth * NUM_TILES_WIDTH / 2}px`
-        block.style.bottom = `${viewportHeight}px`
-    }
+export function start(canvas, setBlockState) {
+        setBlockState(actionType.blockPosition, { x: canvas.width / 2, y: 0})
+        setBlockState(actionType.blockSpeed, BLOCK_INITIAL_SPEED)
 }
 
-export const blockMove = (moveDirection, block, viewportWidth) => {
-    switch (moveDirection) {
-        case blockMoveDirections.left:
-            blockMoveLeft(block, viewportWidth)
-            break
-        case blockMoveDirections.right:
-            blockMoveRight(block, viewportWidth)
-            break
-        default:
-            console.log("Unknown move direction")
-            break
-    }
-}
-
-const blockMoveLeft = (block, viewportWidth) => {
-    if (block) {
-        let tileWidth = parseFloat(viewportWidth / NUM_TILES_WIDTH)
-        let blockNewLeft = parseFloat(block.style.left) - tileWidth
-        if (blockNewLeft > 0) {
-            block.style.left = `${parseFloat(block.style.left) - tileWidth}px`
+export function draw(ctx2D, currentBlock, tileDim) {
+        if (ctx2D) {
+            ctx2D.beginPath()
+            ctx2D.rect(currentBlock.position.x, currentBlock.position.y, tileDim, tileDim)
+            ctx2D.fillStyle = "grba(0, 0, 255, 0.5)"
+            ctx2D.fill()
+            ctx2D.closePath()
         }
-    }
 }
 
-const blockMoveRight = (block, viewportWidth) => {
-    if (block) {
-        let blockRect = block.getBoundingClientRect()
-        let tileWidth = parseFloat(viewportWidth / NUM_TILES_WIDTH)
-        let blockCurrentRight = parseFloat(block.style.left) + blockRect.width
-        let blockNewRight = blockCurrentRight + tileWidth
-        if (blockNewRight < viewportWidth) {
-            block.style.left = `${parseFloat(block.style.left) + tileWidth}px`
+export function finish(ctx2D, canvasWidth, canvasHeight, position, tileDim, timer) {
+        clearInterval(timer)
+        // game.clearCanvas(ctx2D, canvasWidth, canvasHeight)
+        // TODO: correct collision
+        this.draw(ctx2D, position, tileDim)
+}
+
+export function moveSide(canvasWidth, moveDir, setPosition, position, tileDim, setSideMove, sideMove, timer, blockReducer) {
+        // Movo block to left
+        if (moveDir === blockMoveDirection.left) {
+            if (position.x > 0) {
+                setPosition({ x: position.x - tileDim, y: position.y })
+                setSideMove(!sideMove)
+                // Remove timer with old position reference
+                clearInterval(timer)
+            }
+            blockReducer("SET_BLOCK_MOVE", blockMoveDirection.none)
         }
-    }
-}
-
-export const blockNewRotation = currentRotation => {
-    let rotation = currentRotation
-    if (currentRotation >= 270) {
-        rotation = 0
-    }
-    else {
-        rotation += BLOCK_DELTA_ROTATION
-    }
-    return rotation
-}
-
-// TODO: On I block collition does not work
-export const checkRotationCollision = (block, viewportWidth) => {
-
-    if (block) {
-
-        let tileWidth = parseFloat(viewportWidth / NUM_TILES_WIDTH)
-        // Float precision dimensions
-        let blockRect = block.getBoundingClientRect()
-        // Block current right ad left positions
-        let blockCurrentRight = parseFloat(block.style.left) + blockRect.width
-        let blockCurrentLeft = parseFloat(block.style.left)
-
-        // Check if there is an overflow
-        if (blockCurrentRight > viewportWidth) {
-            // if the difference is more than 2 tiles
-            if (blockCurrentRight > (viewportWidth + tileWidth * 2)) {
-                block.style.left = `${viewportWidth - tileWidth * 4.0}px`
+        // Move block to right
+        else if (moveDir === blockMoveDirection.right) {
+            if (position.x + tileDim < canvasWidth) {
+                setPosition({ x: position.x + tileDim, y: position.y })
+                setSideMove(!sideMove)
+                // Remove timer with old position reference
+                clearInterval(timer)
             }
-            // if the difference is more than 1 tile
-            else if (blockCurrentRight > (viewportWidth + tileWidth)) {
-                block.style.left = `${parseFloat(block.style.left) - tileWidth * 2.0}px`
-            }
-            // All others
-            else {
-                block.style.left = `${parseFloat(block.style.left) - tileWidth}px`
-            }
+            blockReducer("SET_BLOCK_MOVE", blockMoveDirection.none)
         }
-        // This is problably not necessary because only left overflow is happening
-        else if (blockCurrentLeft < 0) {
-            if (blockCurrentLeft < -tileWidth) {
-                block.style.left = `${parseFloat(block.style.left) + tileWidth * 2}px`
-            }
-            else {
-                block.style.left = `${parseFloat(block.style.left) + tileWidth}px`
-            }
+}
+
+export function moveDown(canvasHeight, setPosition, position, tileDim, speed, setNewBlock) {
+        if (position.y < canvasHeight - tileDim) {
+            setPosition({ x: position.x, y: position.y += speed })
         }
-    }
+        // TODO: check for collision
+        else {
+            // setPosition({ x: position.x, y: canvasHeight - tileDim })
+            setNewBlock(true)
+        }
 }
 
-export function blockNewSpeed(currentSpeed) {
-    let newSpeed = currentSpeed + BLOCK_DELTA_SPEED
-    return newSpeed
-}
 
-export function getTimerSpeed(gameLevel) {
-    switch (gameLevel) {
-        case 1:
-            return timerSpeeds.level1
-        case 2:
-            return timerSpeeds.level2
-        case 3:
-            return timerSpeeds.level3
-        case 4:
-            return timerSpeeds.level4
-        default:
-            return timerSpeeds.speedUp
-    }
-}
 
-function getBlockType() {
+// export const setBlockInitialPosition = (block, viewportWidth, viewportHeight) => {
+//     if (block) {
+//         let tileWidth = viewportWidth / NUM_TILES_WIDTH
+//         block.style.left = `${tileWidth * NUM_TILES_WIDTH / 2}px`
+//         block.style.bottom = `${viewportHeight}px`
+//     }
+// }
 
-    let blockType = ""
-    let blockNumber = getBlockNumber()
+// export const blockMove = (moveDirection, block, viewportWidth) => {
+//     switch (moveDirection) {
+//         case blockMoveDirection.left:
+//             blockMoveLeft(block, viewportWidth)
+//             break
+//         case blockMoveDirections.right:
+//             blockMoveRight(block, viewportWidth)
+//             break
+//         default:
+//             console.log("Unknown move direction")
+//             break
+//     }
+// }
 
-    // Return the blockType that is going to be used as a css class to define the block layout
-    switch(blockNumber) {
-        case 1:
-            blockType = blockTypes.one.name
-            return blockType
-        case 2:
-            blockType = blockTypes.two.name
-            return blockType
-        case 3:
-            blockType = blockTypes.three.name
-            return blockType
-        case 4:
-            blockType = blockTypes.four.name
-            return blockType
-        case 5:
-            blockType = blockTypes.five.name
-            return blockType
-        case 6:
-            blockType = blockTypes.six.name
-            return blockType
-        case 7:
-            blockType = blockTypes.seven.name
-            return blockType
-        default:
-            console.log("Unknown block number: " + blockNumber)
-            break
-    }
-}
+// const blockMoveLeft = (block, viewportWidth) => {
+//     if (block) {
+//         let tileWidth = parseFloat(viewportWidth / NUM_TILES_WIDTH)
+//         let blockNewLeft = parseFloat(block.style.left) - tileWidth
+//         if (blockNewLeft > 0) {
+//             block.style.left = `${parseFloat(block.style.left) - tileWidth}px`
+//         }
+//     }
+// }
 
-// Return the block number used to define the block type
-function getBlockNumber() {
-    const min = 1, max = 6
-    let blockNum = Math.round(Math.random() * max) + min
-    return blockNum
-}
+// const blockMoveRight = (block, viewportWidth) => {
+//     if (block) {
+//         let blockRect = block.getBoundingClientRect()
+//         let tileWidth = parseFloat(viewportWidth / NUM_TILES_WIDTH)
+//         let blockCurrentRight = parseFloat(block.style.left) + blockRect.width
+//         let blockNewRight = blockCurrentRight + tileWidth
+//         if (blockNewRight < viewportWidth) {
+//             block.style.left = `${parseFloat(block.style.left) + tileWidth}px`
+//         }
+//     }
+// }
+
+// export const blockNewRotation = currentRotation => {
+//     let rotation = currentRotation
+//     if (currentRotation >= 270) {
+//         rotation = 0
+//     }
+//     else {
+//         rotation += BLOCK_DELTA_ROTATION
+//     }
+//     return rotation
+// }
+
+// // TODO: On I block collition does not work
+// export const checkRotationCollision = (block, viewportWidth) => {
+
+//     if (block) {
+
+//         let tileWidth = parseFloat(viewportWidth / NUM_TILES_WIDTH)
+//         // Float precision dimensions
+//         let blockRect = block.getBoundingClientRect()
+//         // Block current right ad left positions
+//         let blockCurrentRight = parseFloat(block.style.left) + blockRect.width
+//         let blockCurrentLeft = parseFloat(block.style.left)
+
+//         // Check if there is an overflow
+//         if (blockCurrentRight > viewportWidth) {
+//             // if the difference is more than 2 tiles
+//             if (blockCurrentRight > (viewportWidth + tileWidth * 2)) {
+//                 block.style.left = `${viewportWidth - tileWidth * 4.0}px`
+//             }
+//             // if the difference is more than 1 tile
+//             else if (blockCurrentRight > (viewportWidth + tileWidth)) {
+//                 block.style.left = `${parseFloat(block.style.left) - tileWidth * 2.0}px`
+//             }
+//             // All others
+//             else {
+//                 block.style.left = `${parseFloat(block.style.left) - tileWidth}px`
+//             }
+//         }
+//         // This is problably not necessary because only left overflow is happening
+//         else if (blockCurrentLeft < 0) {
+//             if (blockCurrentLeft < -tileWidth) {
+//                 block.style.left = `${parseFloat(block.style.left) + tileWidth * 2}px`
+//             }
+//             else {
+//                 block.style.left = `${parseFloat(block.style.left) + tileWidth}px`
+//             }
+//         }
+//     }
+// }
+
+// export function blockNewSpeed(currentSpeed) {
+//     let newSpeed = currentSpeed + BLOCK_DELTA_SPEED
+//     return newSpeed
+// }
+
+// export function getTimerSpeed(gameLevel) {
+//     switch (gameLevel) {
+//         case 1:
+//             return timerSpeeds.level1
+//         case 2:
+//             return timerSpeeds.level2
+//         case 3:
+//             return timerSpeeds.level3
+//         case 4:
+//             return timerSpeeds.level4
+//         default:
+//             return timerSpeeds.speedUp
+//     }
+// }
+
+// function getBlockType() {
+
+//     let blockType = ""
+//     let blockNumber = getBlockNumber()
+
+//     // Return the blockType that is going to be used as a css class to define the block layout
+//     switch(blockNumber) {
+//         case 1:
+//             blockType = blockTypes.one.name
+//             return blockType
+//         case 2:
+//             blockType = blockTypes.two.name
+//             return blockType
+//         case 3:
+//             blockType = blockTypes.three.name
+//             return blockType
+//         case 4:
+//             blockType = blockTypes.four.name
+//             return blockType
+//         case 5:
+//             blockType = blockTypes.five.name
+//             return blockType
+//         case 6:
+//             blockType = blockTypes.six.name
+//             return blockType
+//         case 7:
+//             blockType = blockTypes.seven.name
+//             return blockType
+//         default:
+//             console.log("Unknown block number: " + blockNumber)
+//             break
+//     }
+// }
+
+// // Return the block number used to define the block type
+// function getBlockNumber() {
+//     const min = 1, max = 6
+//     let blockNum = Math.round(Math.random() * max) + min
+//     return blockNum
+// }
--- src/libs/game.js
+++ src/libs/game.js
@@ -1,75 +1,41 @@
-import { BLOCK_INITIAL_SPEED, blockMoveDirections } from '../globals.js'
+import { start as blockStart, draw as blockDraw } from './block.js'
+import { TIMER_SPEED } from '../globals.js'
+import { setBlockState } from '../actions/blockActions.js'
 
-export const block = {
+export function start(setTimer, ctx2D, wall, currentBlock, canvas, tileDim) {
+    blockStart(canvas, setBlockState)
+    setTimer(setInterval(draw.bind(null, ctx2D, wall, currentBlock, canvas, tileDim), TIMER_SPEED))
+}
 
-    draw: function(ctx2D, position, tileDim) {
-        ctx2D.beginPath()
-        ctx2D.rect(position.x, position.y, tileDim, tileDim)
-        ctx2D.fillStyle = "grba(0, 0, 255, 0.5)"
-        ctx2D.fill()
-        ctx2D.closePath()
-    },
+export function draw(ctx2D, wall, currentBlock, canvas, tileDim) {
+    clearCanvas(ctx2D, canvas.width, canvas.height)
+    blockDraw(ctx2D, currentBlock, tileDim)
+    // block.moveDown(canvas.height, setPosition, position, tileDim, speed, setNewBlock)
+    console.log("need to clear interval")
+}
 
-    finish: function(ctx2D, canvasWidth, canvasHeight, position, tileDim, timer) {
-        clearInterval(timer)
-        // game.clearCanvas(ctx2D, canvasWidth, canvasHeight)
-        // TODO: correct collision
-        this.draw(ctx2D, position, tileDim)
-    },
+// drawWall: function(blocks, position, tileDim) {
+//     for (block in blocks) {
+//         ctx2D.beginPath()
+//         ctx2D.rect(position.x, position.y, tileDim, tileDim)
+//         ctx2D.fillStyle = "grba(0, 0, 255, 0.5)"
+//         ctx2D.fill()
+//         ctx2D.closePath()
+//     }
+// },
 
-    init: function(canvasWidth, setPostion, setSpeed, speedState) {
-        setPostion({ x: canvasWidth / 2, y: 0})
-        setSpeed(BLOCK_INITIAL_SPEED)
-        speedState("SET_BLOCK_SPEED", BLOCK_INITIAL_SPEED)
-    },
-
-    moveSide: function(canvasWidth, moveDir, setPosition, position, tileDim, setSideMove, sideMove, timer, blockReducer) {
-        // Movo block to left
-        if (moveDir === blockMoveDirections.left) {
-            if (position.x > 0) {
-                setPosition({ x: position.x - tileDim, y: position.y })
-                setSideMove(!sideMove)
-                // Remove timer with old position reference
-                clearInterval(timer)
-            }
-            blockReducer("SET_BLOCK_MOVE", blockMoveDirections.none)
-        }
-        // Move block to right
-        else if (moveDir === blockMoveDirections.right) {
-            if (position.x + tileDim < canvasWidth) {
-                setPosition({ x: position.x + tileDim, y: position.y })
-                setSideMove(!sideMove)
-                // Remove timer with old position reference
-                clearInterval(timer)
-            }
-            blockReducer("SET_BLOCK_MOVE", blockMoveDirections.none)
-        }
-    },
-
-    moveDown: function(canvasHeight, setPosition, position, tileDim, speed, setNewBlock) {
-        if (position.y < canvasHeight - tileDim) {
-            setPosition({ x: position.x, y: position.y += speed })
-        }
-        // TODO: check for collision
-        else {
-            // setPosition({ x: position.x, y: canvasHeight - tileDim })
-            setNewBlock(true)
-        }
+export function clearCanvas(ctx2D, canvas) {
+    if (ctx2D) {
+        ctx2D.clearRect(0, 0, canvas.width, canvas.height)
     }
 }
 
-export const game = {
-    clearCanvas: function(ctx2D, canvasWidth, canvasHeight) {
-        ctx2D.clearRect(0, 0, canvasWidth, canvasHeight)
-    },
+export function update() {
 
-    draw: function(blocks, position, tileDim) {
-        for (block in blocks) {
-            ctx2D.beginPath()
-            ctx2D.rect(position.x, position.y, tileDim, tileDim)
-            ctx2D.fillStyle = "grba(0, 0, 255, 0.5)"
-            ctx2D.fill()
-            ctx2D.closePath()
-        }
-    }
+}
+
+
+export function finish(timer, ctx2D, canvas) {
+    clearInterval(timer)
+    clearCanvas(ctx2D, canvas)
 }
