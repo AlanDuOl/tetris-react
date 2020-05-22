340752f1ddb4739d31c4365aa5e4771314a0aa53
alanduarte90 <alanduarte90@gmail.com>
2020-05-22 18:26:47 +0100

    Update block start logic

--- src/components/Display.js
+++ src/components/Display.js
@@ -5,7 +5,7 @@ import { setBlockState } from '../actions/blockActions'
 import { setGameState } from '../actions/gameActions'
 import { BLOCK_INITIAL_SPEED, blockMoveDirection, NUM_TILES_WIDTH } from '../globals.js'
 import { gameStart, gameFinish, gamePause, gameResume, gameUpdate } from '../libs/game.js'
-import { blockMoveSide, blockSpeedUp } from '../libs/block.js'
+import { blockMoveSide, blockSpeedUp, blockStart } from '../libs/block.js'
 
 
 function Display(props) {
@@ -14,7 +14,6 @@ function Display(props) {
     const [canvas, setCanvas] = useState({ width: 0, height: 0, tileDim: 0 })
     const [ctx2D, setCtx2D] = useState(null)
     const [timer, setTimer] = useState(0)
-    const [initialBlock, setInitialBlock] = useState({ })
     const [speedChange, setSpeedChange] = useState(false)
     const [wall, setWall] = useState([])
 
@@ -23,13 +22,13 @@ function Display(props) {
         let canvas = document.getElementById("display-viewport")
         setCanvas({ width: canvas.width, height: canvas.height, tileDim: canvas.width / NUM_TILES_WIDTH })
         setCtx2D(canvas.getContext("2d"))
-        setInitialBlock({ position: { x: canvas.width / 2, y: - canvas.width / NUM_TILES_WIDTH }, speed: BLOCK_INITIAL_SPEED })
+        blockStart(canvas, props.setBlockState)
     }, [])
 
     // Start/End
     useEffect(() => {
         if (props.gameReducer.gameOn) {
-            gameStart(setTimer, ctx2D, wall, initialBlock, canvas, props.setBlockState)
+            gameStart(setTimer, ctx2D, wall, props.blockReducer, canvas, props.setBlockState)
         }
         else {
             gameFinish(timer, ctx2D, canvas)
