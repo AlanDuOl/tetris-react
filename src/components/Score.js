1bd25e38c6d25a49a4c46522a72d424c791c5e23
alanduarte90 <alanduarte90@gmail.com>
2020-06-03 15:43:50 +0100

    Start to implement update score

--- src/components/Score.js
+++ src/components/Score.js
@@ -1,25 +1,40 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { connect } from 'react-redux'
 import '../css/Score.css'
 
-function Score() {
+function Score(props) {
+
+    const [score, setScore] = useState(0)
+    const [record, setRecord] = useState(0)
+    const [level, setLevel] = useState(0)
+
+    useEffect(() => {
+        setScore(props.gameReducer.score)
+        setLevel(props.gameReducer.level)
+    }, [props.gameReducer.score])
+
     return (
         <section id="score">
             <div id="score-container">
                 <div className="score-element">
                     <label htmlFor="score-current">Points:</label>
-                    <output id="score-current"></output>
+                    <output id="score-current">{score}</output>
                 </div>
                 <div className="score-element">
                     <label htmlFor="score-level">Level:</label>
-                    <output id="score-level"></output>
+                    <output id="score-level">{level}</output>
                 </div>
                 <div className="score-element">
                     <label htmlFor="score-record">Record:</label>
-                    <output id="score-record"></output>
+                    <output id="score-record">{record}</output>
                 </div>
             </div>
         </section>
     )
 }
 
-export default Score
+const mapStateToProps = state => ({
+    ...state
+})
+
+export default connect(mapStateToProps, null)(Score)
