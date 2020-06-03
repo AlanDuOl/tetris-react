f87925d97a6977a027482bfc8141e67a7dab9225
alanduarte90 <alanduarte90@gmail.com>
2020-06-03 13:55:27 +0100

    Remove save/load btns and adjust layout

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -42,17 +42,11 @@ function Menu(props) {
 
     return (
         <section id="menu">
-            <div className="menu-element" id="menu-element-1">
+            <div className="menu-element">
                 <button type="button" className="menu-btn" id="menu-btn-start" onClick={toggleStart}>{ props.gameReducer.gameOn ? "Finish" : "Start" }</button>
             </div>
-            <div className="menu-element" id="menu-element-2">
+            <div className="menu-element">
                 <button type="button" className="menu-btn" id="menu-btn-pause" onClick={togglePause}>{ props.gameReducer.gamePaused ? "Resume" : "Pause" }</button>
-            </div>
-            <div className="menu-element" id="menu-element-3">
-                <button type="button" className="menu-btn" id="menu-btn-save">Save</button>
-            </div>
-            <div className="menu-element" id="menu-element-4">
-                <button type="button" className="menu-btn" id="menu-btn-load">Load</button>
             </div>
         </section>
     )
